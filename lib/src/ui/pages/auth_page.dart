092b282f87be426231532450f3328ec0a1bbc9a1
Rithik Jain <rithik.jain3006@gmail.com>
2020-09-04 17:21:01 +0530

    Auth working basix

--- lib/src/ui/pages/auth_page.dart
+++ lib/src/ui/pages/auth_page.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:news_summarizer/src/providers/theme_provider.dart';
 import 'package:news_summarizer/src/ui/pages/home_page.dart';
@@ -18,6 +19,7 @@ class _AuthPageState extends State<AuthPage> {
   bool _isLoading = false;
   bool _isOTPWidgetVisible = false;
   String _otpString = "";
+  String _verificationCode;
   final TextEditingController _phoneController = TextEditingController();
 
   final Future<FirebaseApp> _initialization = Firebase.initializeApp();
@@ -45,6 +47,7 @@ class _AuthPageState extends State<AuthPage> {
       setState(() {
         _isLoading = false;
         _isOTPWidgetVisible = true;
+        _verificationCode = verificationId;
       });
     };
 
@@ -66,7 +69,7 @@ class _AuthPageState extends State<AuthPage> {
 
     await _auth.verifyPhoneNumber(
       phoneNumber: "+91" + _phoneController.text,
-      timeout: Duration(seconds: 60),
+      timeout: Duration(seconds: 90),
       verificationCompleted: verificationCompleted,
       verificationFailed: verificationFailed,
       codeSent: codeSent,
@@ -75,6 +78,58 @@ class _AuthPageState extends State<AuthPage> {
         print("Timeout");
       },
     );
+  }
+
+  Future<void> signInPhoneNumber(String smsCode) async {
+    final AuthCredential credential = PhoneAuthProvider.credential(
+      verificationId: _verificationCode,
+      smsCode: smsCode,
+    );
+    try {
+      var authResult =
+          await FirebaseAuth.instance.signInWithCredential(credential);
+      User user = authResult.user;
+      if (user != null) {
+        setState(() {
+          _isLoading = false;
+          Navigator.popAndPushNamed(context, HomeWidget.routename);
+        });
+      }
+    } on PlatformException catch (err) {
+      if (err.code == "ERROR_INVALID_VERIFICATION_CODE") {
+        Get.snackbar(
+          "Error",
+          "Invalid OTP",
+          backgroundColor: Colors.red,
+          colorText: Colors.white,
+          snackPosition: SnackPosition.BOTTOM,
+        );
+      } else {
+        Get.snackbar(
+          "Error",
+          "Something went wrong!",
+          backgroundColor: Colors.red,
+          colorText: Colors.white,
+          snackPosition: SnackPosition.BOTTOM,
+        );
+      }
+      setState(() {
+        _isLoading = false;
+      });
+    } catch (e) {
+      print(e);
+      _isLoading = false;
+      Get.snackbar(
+        "Error",
+        "Something went wrong!",
+        backgroundColor: Colors.red,
+        colorText: Colors.white,
+        snackPosition: SnackPosition.BOTTOM,
+      );
+      setState(() {
+        _isLoading = false;
+      });
+    }
   }
 
   @override
@@ -105,118 +160,127 @@ class _AuthPageState extends State<AuthPage> {
         future: _initialization,
         builder: (context, snapshot) => (snapshot.connectionState ==
                 ConnectionState.done)
-            ? Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  SizedBox(height: 20),
-                  Container(
-                    margin: EdgeInsets.all(24),
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                      "Let's get you started!",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 24,
+            ? SingleChildScrollView(
+                physics: BouncingScrollPhysics(),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    SizedBox(height: 20),
+                    Container(
+                      margin: EdgeInsets.all(24),
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        "Let's get you started!",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 24,
+                        ),
                       ),
                     ),
-                  ),
-                  SizedBox(height: 50),
-                  Padding(
-                    padding: EdgeInsets.all(24),
-                    child: Form(
-                      key: _formkey,
+                    SizedBox(height: 50),
+                    Padding(
+                      padding: EdgeInsets.all(24),
+                      child: Form(
+                        key: _formkey,
+                        child: Column(
+                          children: [
+                            TextFormField(
+                              enabled: !_isOTPWidgetVisible,
+                              validator: (value) {
+                                if (value.length < 10) {
+                                  return 'Please enter a valid number!';
+                                }
+                                return null;
+                              },
+                              controller: _phoneController,
+                              maxLength: 10,
+                              keyboardType: TextInputType.number,
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(),
+                                labelText: 'Phone Number',
+                                hintText: '8197513721',
+                                icon:
+                                    Icon(Icons.phone, color: Color(0xff3B916E)),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Visibility(
+                      visible: _isOTPWidgetVisible,
                       child: Column(
                         children: [
-                          TextFormField(
-                            enabled: !_isLoading,
-                            validator: (value) {
-                              if (value.length < 10) {
-                                return 'Please enter a valid number!';
-                              }
-                              return null;
-                            },
-                            controller: _phoneController,
-                            maxLength: 10,
-                            keyboardType: TextInputType.number,
-                            decoration: InputDecoration(
-                              border: OutlineInputBorder(),
-                              labelText: 'Phone Number',
-                              hintText: '8197513721',
-                              icon: Icon(Icons.phone, color: Color(0xff3B916E)),
+                          Container(
+                            margin: EdgeInsets.symmetric(
+                              horizontal: 24,
+                              vertical: 24,
+                            ),
+                            alignment: Alignment.centerLeft,
+                            child: Text("Enter the OTP received"),
+                          ),
+                          SizedBox(height: 20),
+                          Container(
+                            margin: EdgeInsets.symmetric(horizontal: 32),
+                            child: PinCodeTextField(
+                              animationType: AnimationType.slide,
+                              textInputType: TextInputType.number,
+                              length: 6,
+                              backgroundColor: Colors.transparent,
+                              enabled: !_isLoading,
+                              textStyle: TextStyle(
+                                color: (themeProvider.theme ==
+                                        themeProvider.darkTheme)
+                                    ? Colors.white
+                                    : Colors.black,
+                                fontSize: 18,
+                              ),
+                              pinTheme: PinTheme(
+                                selectedColor: Theme.of(context).accentColor,
+                                activeColor: Theme.of(context).accentColor,
+                                inactiveColor: Theme.of(context).accentColor,
+                                fieldWidth: 40,
+                              ),
+                              onCompleted: (value) {
+                                setState(() {
+                                  _otpString = value;
+                                });
+                              },
+                              onChanged: (value) {
+                                setState(() {});
+                              },
                             ),
                           ),
+                          SizedBox(height: 40),
                         ],
                       ),
                     ),
-                  ),
-                  Visibility(
-                    visible: _isOTPWidgetVisible,
-                    child: Column(
-                      children: [
-                        Container(
-                          margin: EdgeInsets.symmetric(
-                            horizontal: 24,
-                            vertical: 24,
-                          ),
-                          alignment: Alignment.centerLeft,
-                          child: Text("Enter the OTP received"),
-                        ),
-                        SizedBox(height: 20),
-                        Container(
-                          margin: EdgeInsets.symmetric(horizontal: 32),
-                          child: PinCodeTextField(
-                            animationType: AnimationType.slide,
-                            textInputType: TextInputType.number,
-                            length: 6,
-                            backgroundColor: Colors.transparent,
-                            textStyle: TextStyle(
-                              color: (themeProvider.theme ==
-                                      themeProvider.darkTheme)
-                                  ? Colors.white
-                                  : Colors.black,
-                              fontSize: 18,
-                            ),
-                            pinTheme: PinTheme(
-                              selectedColor: Theme.of(context).accentColor,
-                              activeColor: Theme.of(context).accentColor,
-                              inactiveColor: Theme.of(context).accentColor,
-                              fieldWidth: 40,
-                            ),
-                            onCompleted: (value) {
-                              setState(() {
-                                _otpString = value;
-                              });
-                            },
-                            onChanged: (value) {
-                              setState(() {});
-                            },
+                    Container(
+                      margin: EdgeInsets.all(24),
+                      width: 100,
+                      height: 45,
+                      child: MaterialButton(
+                        child: Text(
+                          "NEXT",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Theme.of(context).primaryColor,
                           ),
                         ),
-                        SizedBox(height: 40),
-                      ],
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.all(24),
-                    width: 100,
-                    height: 45,
-                    child: MaterialButton(
-                      child: Text(
-                        "NEXT",
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: Theme.of(context).primaryColor,
-                        ),
+                        onPressed: () {
+                          if (_isOTPWidgetVisible) {
+                            signInPhoneNumber(_otpString);
+                          } else {
+                            if (_formkey.currentState.validate()) {
+                              initPhoneAuth(context);
+                            }
+                          }
+                        },
+                        color: Theme.of(context).accentColor,
                       ),
-                      onPressed: () {
-                        if (_formkey.currentState.validate()) {
-                          initPhoneAuth(context);
-                        }
-                      },
-                      color: Theme.of(context).accentColor,
                     ),
-                  ),
-                ],
+                  ],
+                ),
               )
             : Container(),
       ),
