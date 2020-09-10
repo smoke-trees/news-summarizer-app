e9e783a6b6daa428804d78a143fb9132f93789ac
Rithik Jain <rithik.jain3006@gmail.com>
2020-09-10 11:43:18 +0530

    AppBar: Added Search functionality

--- lib/src/ui/pages/news_container_page.dart
+++ lib/src/ui/pages/news_container_page.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
+import 'package:news_summarizer/src/providers/api_provider.dart';
 import 'package:news_summarizer/src/ui/pages/news_page.dart';
 import 'package:news_summarizer/src/ui/pages/preferences_page.dart';
+import 'package:news_summarizer/src/ui/pages/search_page.dart';
 import 'package:news_summarizer/src/ui/widgets/theme_dialog.dart';
 import 'package:news_summarizer/src/utils/constants.dart';
+import 'package:provider/provider.dart';
 
 class NewsContainerPage extends StatefulWidget {
   @override
@@ -13,6 +16,7 @@ class NewsContainerPage extends StatefulWidget {
 class _NewsContainerPageState extends State<NewsContainerPage>
     with AutomaticKeepAliveClientMixin<NewsContainerPage> {
   var _newsFeeds;
+  bool _isSearchActive = false;
 
   @override
   void initState() {
@@ -35,6 +39,64 @@ class _NewsContainerPageState extends State<NewsContainerPage>
     );
   }
 
+  Widget _searchAppBar(BuildContext context) {
+    final apiProvider = Provider.of<ApiProvider>(context);
+
+    return AppBar(
+      title: Row(
+        children: [
+          Icon(Icons.search),
+          Expanded(
+            child: Container(
+              margin: EdgeInsets.all(8),
+              height: 45,
+              child: TextField(
+                textInputAction: TextInputAction.search,
+                onSubmitted: (value) {
+                  apiProvider.setSearchTerm(value);
+                  setState(() {
+                    _isSearchActive = false;
+                  });
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => SearchPage()),
+                  );
+                },
+                autofocus: true,
+                textAlign: TextAlign.center,
+                textAlignVertical: TextAlignVertical.bottom,
+                decoration: InputDecoration(
+                  hintText: "Search for any news topic!",
+                  filled: true,
+                  fillColor: Theme.of(context).cardColor,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: BorderSide(
+                      width: 0,
+                      style: BorderStyle.none,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          InkWell(
+            child: Text(
+              "Cancel",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 14,
+              ),
+            ),
+            onTap: () => setState(() {
+              _isSearchActive = false;
+            }),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     super.build(context);
@@ -42,59 +104,66 @@ class _NewsContainerPageState extends State<NewsContainerPage>
       length: _newsFeeds.length,
       child: Scaffold(
         backgroundColor: Colors.transparent,
-        appBar: AppBar(
-          centerTitle: true,
-          leading: Icon(Icons.search),
-          title: Text(
-            'News',
-            style: TextStyle(
-              fontSize: 22,
-              fontWeight: FontWeight.bold,
-              color: Theme.of(context).accentColor,
-            ),
-          ),
-          actions: [
-            PopupMenuButton(
-              onSelected: (value) async {
-                switch (value) {
-                  case 'Change Theme':
-                    _showThemeDialog(context);
-                    break;
-                  case 'Change News Preferences':
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => PreferencesPage(),
-                      ),
-                    );
-                    break;
-                }
-              },
-              itemBuilder: (context) {
-                return {'Change Theme', 'Change News Preferences'}
-                    .map((String choice) {
-                  return PopupMenuItem<String>(
-                    value: choice,
-                    child: Text(choice),
-                  );
-                }).toList();
-              },
-            ),
-          ],
-          bottom: TabBar(
-            tabs: List.generate(
-              _newsFeeds.length,
-              (index) => Tab(
-                text: _newsFeeds[index]
-                    .toString()
-                    .split('.')
-                    .last
-                    .replaceAll("_", " "),
-              ),
-            ),
-            isScrollable: true,
-          ),
-        ),
+        appBar: (!_isSearchActive)
+            ? AppBar(
+                centerTitle: true,
+                leading: InkWell(
+                  child: Icon(Icons.search),
+                  onTap: () => setState(() {
+                    _isSearchActive = true;
+                  }),
+                ),
+                title: Text(
+                  'News',
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                    color: Theme.of(context).accentColor,
+                  ),
+                ),
+                actions: [
+                  PopupMenuButton(
+                    onSelected: (value) async {
+                      switch (value) {
+                        case 'Change Theme':
+                          _showThemeDialog(context);
+                          break;
+                        case 'Change News Preferences':
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => PreferencesPage(),
+                            ),
+                          );
+                          break;
+                      }
+                    },
+                    itemBuilder: (context) {
+                      return {'Change Theme', 'Change News Preferences'}
+                          .map((String choice) {
+                        return PopupMenuItem<String>(
+                          value: choice,
+                          child: Text(choice),
+                        );
+                      }).toList();
+                    },
+                  ),
+                ],
+                bottom: TabBar(
+                  tabs: List.generate(
+                    _newsFeeds.length,
+                    (index) => Tab(
+                      text: _newsFeeds[index]
+                          .toString()
+                          .split('.')
+                          .last
+                          .replaceAll("_", " "),
+                    ),
+                  ),
+                  isScrollable: true,
+                ),
+              )
+            : _searchAppBar(context),
         body: TabBarView(
           children: List.generate(
             _newsFeeds.length,
